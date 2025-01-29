67024264d1ff9a46286061afd649b81bbc7e1aad
Adrian Schröter <adrian@suse.de>
2025-01-29 11:16:32 +0100

    agama: add options for volumeid, publisher and keeping bootloader

--- src/productcomposer/cli.py
+++ src/productcomposer/cli.py
@@ -439,7 +439,11 @@ def create_tree(outdir, product_base_dir, yml, pool, flavor, vcs=None, disturl=N
         note("Export main tree into agama iso file")
         import glob
         agamas = glob.glob(f"/usr/libexec/base-isos/{yml['iso']['base']}*.iso", recursive=True)
-        args = ['xorriso', '-dev', agamas[0], '-map', workdirectories[0], '/install', '-outdev', workdirectories[0] + '.install.iso']
+        args = ['xorriso', '-boot_image', 'any', 'keep', '-indev', agamas[0], '-map', workdirectories[0], '/install', '-outdev', workdirectories[0] + '.install.iso']
+        if 'publisher' in yml['iso'] and yml['iso']['publisher'] is not None:
+            args += ['-publisher', yml['iso']['publisher']]
+        if 'volume_id' in yml['iso'] and yml['iso']['volume_id'] is not None:
+            args += ['-volid', yml['iso']['volume_id']]
         run_helper(args, failmsg="Adding tree to agama image")
 
 
