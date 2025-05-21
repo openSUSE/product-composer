6f56d1a0e0c120e362e45b83d3ff1a1871a1bd31
atartamo <atartamo@suse.de>
2025-05-21 21:12:04 +0200

    added parsers unit tests

---
+++ tests/unit/parsers/test_eulasparser.py
@@ -0,0 +1,13 @@
+import pytest
+from productcomposer.parsers.eulasparser import parse_eulas
+
+def test_eulasparser_ok():
+    eulas = {}
+    parse_eulas("./tests/assets/eulas", eulas)
+    assert len(eulas) == 3
+
+def test_eulasparser_wrong_path():
+    eulas = {}
+    parse_eulas("./tests/assets/xulas", eulas)
+    assert len(eulas) == 0
+
