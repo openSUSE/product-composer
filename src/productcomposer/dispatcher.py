ab3a3e8f496850e4336ed3de08b08d7e66af2afe
Michael Schroeder <mls@suse.de>
2025-06-04 16:06:16 +0200

    Remove debug print statements

--- src/productcomposer/dispatcher.py
+++ src/productcomposer/dispatcher.py
@@ -1,9 +1,7 @@
 from .commands import COMMANDS
 
 def dispatch(args):
-    print(COMMANDS)
     cmd_class = COMMANDS.get(args.command)
-    print(args.command)
     if not cmd_class:
         raise ValueError(f"Unknown command: {args.command}")
     cmd_instance = cmd_class()
