e23e739a31c3ab31c4b7cb7f909425d04fe66480
Dirk Müller <dmueller@suse.com>
2025-06-05 07:35:47 +0200

    Cleanup verification test for pydantic switch

--- src/productcomposer/commands/verify.py
+++ src/productcomposer/commands/verify.py
@@ -1,7 +1,7 @@
 from ..parsers.yamlparser import parse_yaml
 from . import register
-from ..utils.loggerutils import (die, note)
-from ..utils.rpmutils import (create_package_set)
+from ..utils.loggerutils import die, note
+from ..utils.rpmutils import create_package_set
 from ..core.Pool import Pool
 
 # global db for eulas
@@ -11,18 +11,19 @@ supportstatus = {}
 # per package override via supportstatus.txt file
 supportstatus_override = {}
 
-@register("verify")
+
+@register('verify')
 class VerifyCommand:
     def run(self, args):
         result = self.verify(args)
 
     def verify_flavor(self, filename, flavor):
         yml = parse_yaml(filename, flavor)
-        if 'architectures' not in yml or not yml['architectures']:
-            if flavor:
-                die(f'No architecture defined for flavor {flavor}')
-            else:
-                die('No architecture defined and no flavor.')
+        if not flavor and not yml['architectures']:
+            die('No architecture defined and no flavor.')
+        if not yml['architectures']:
+            die(f'No architecture defined for flavor {flavor}')
+
         # check package sets
         for arch in yml['architectures']:
             pool = Pool()
@@ -31,7 +32,7 @@ class VerifyCommand:
             for pkgset_name in yml['unpack']:
                 create_package_set(yml, arch, flavor, pkgset_name, pool=pool)
         return yml.get('flavors')
-        
+
     def verify(self, args):
         flavors = self.verify_flavor(args.filename, args.flavor)
         if args.flavor == None:
