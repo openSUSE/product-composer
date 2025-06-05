9e2145cf98d2737a6ba4f9338f8a3d507c735120
Dirk Müller <dmueller@suse.com>
2025-06-05 07:37:07 +0200

    formatting

--- tests/unit/parsers/test_yamlparser.py
+++ tests/unit/parsers/test_yamlparser.py
@@ -2,46 +2,63 @@ import pytest
 from schema import Schema, And, Or, Optional, SchemaError, SchemaMissingKeyError
 from productcomposer.parsers.yamlparser import parse_yaml
 
+
 def test_yamlparser_ok():
-    yml = parse_yaml('./tests/assets/tomls/Backports.productcompose', 'backports_x86_64')
+    yml = parse_yaml(
+        './tests/assets/tomls/Backports.productcompose', 'backports_x86_64'
+    )
     assert len(yml['flavors']) == 4
 
 
 def test_yamlparser_wrong_schema(capsys):
     with pytest.raises(SystemExit) as excinfo:
-        yml = parse_yaml('./tests/assets/tomls/UnsupportedSchemaVer.productcompose', 'backports_x86_64')
+        yml = parse_yaml(
+            './tests/assets/tomls/UnsupportedSchemaVer.productcompose',
+            'backports_x86_64',
+        )
 
     assert excinfo.type == SystemExit
     assert excinfo.value.code == 1
 
     captured = capsys.readouterr()
-    assert "ERROR: Unsupported product composer schema: 0.3" in captured.out
+    assert 'ERROR: Unsupported product composer schema: 0.3' in captured.out
 
 
 def test_yamlparser_missing_schema(capsys):
     with pytest.raises(SystemExit) as excinfo:
-        yml = parse_yaml('./tests/assets/tomls/MissingSchemaVer.productcompose', 'backports_x86_64')
+        yml = parse_yaml(
+            './tests/assets/tomls/MissingSchemaVer.productcompose', 'backports_x86_64'
+        )
 
     assert excinfo.type == SystemExit
     assert excinfo.value.code == 1
 
     captured = capsys.readouterr()
-    assert "ERROR: missing product composer schema" in captured.out    
+    assert 'ERROR: missing product composer schema' in captured.out
+
 
 def test_yamlparser_invalid_schema():
     with pytest.raises(SchemaMissingKeyError, match="Missing key: 'product-type'"):
-        yml = parse_yaml('./tests/assets/tomls/InvalidSchema.productcompose', 'backports_x86_64')
+        yml = parse_yaml(
+            './tests/assets/tomls/InvalidSchema.productcompose', 'backports_x86_64'
+        )
+
 
 def test_yamlparser_flavor_notfound(capsys):
     with pytest.raises(SystemExit) as excinfo:
-        yml = parse_yaml('./tests/assets/tomls/Backports.productcompose', 'ports_x86_64')
+        yml = parse_yaml(
+            './tests/assets/tomls/Backports.productcompose', 'ports_x86_64'
+        )
 
     assert excinfo.type == SystemExit
     assert excinfo.value.code == 1
 
     captured = capsys.readouterr()
-    assert "ERROR: Flavor not found" in captured.out            
+    assert 'ERROR: Flavor not found' in captured.out
+
 
 def test_yamlparser_invalid_buildoption(capsys):
     with pytest.raises(SchemaError, match="Key 'build_options' error"):
-        yml = parse_yaml('./tests/assets/tomls/InvalidBuildOption.productcompose', 'backports_x86_64')
+        yml = parse_yaml(
+            './tests/assets/tomls/InvalidBuildOption.productcompose', 'backports_x86_64'
+        )
