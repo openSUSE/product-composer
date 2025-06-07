f34ec6c1e9ded24922d7f38b9f81f1baa87753c4
atartamo <atartamo@suse.de>
2025-06-07 20:47:49 +0200

    fixed path

--- tests/unit/core/test_updateinfo.py
+++ tests/unit/core/test_updateinfo.py
@@ -1,13 +1,8 @@
 import pytest
 from productcomposer.core.Updateinfo import Updateinfo
 
-def test_updateinfo_missing_file():
-    location = "./tests/assets/updateinfox.xml"
-    with pytest.raises(FileNotFoundError):
-        ui = Updateinfo(location)
-
 def test_updateinfo_ok():
-    location = "./tests/assets/updateinfo.xml"
+    location = "tests/assets/updateinfo.xml"
     ui = Updateinfo(location)
     found = False
     for update in ui.root.findall('update'):
@@ -15,3 +10,8 @@ def test_updateinfo_ok():
         if update_id is not None and update_id.text == "openSUSE-2024-153":
             found = True
     assert found
+
+def test_updateinfo_missing_file():
+    location = "tests/assets/updateinfox.xml"
+    with pytest.raises(FileNotFoundError):
+        ui = Updateinfo(location)    
