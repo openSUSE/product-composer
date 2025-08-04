81ab29fc87031046f2d8ab0d1c59a0d53e3545f1
Adrian Schröter <adrian@suse.de>
2025-08-04 09:16:28 +0200

    Add appstream meta data to repomd.xml

--- src/productcomposer/createartifacts/createappstream.py
+++ src/productcomposer/createartifacts/createappstream.py
@@ -2,17 +2,45 @@ import os
 
 from ..config import verbose_level
 from ..utils.runhelper import run_helper
+from ..wrappers import ModifyrepoWrapper
 
 OPENSUSE_APPSTREAM_PROCESS = '/usr/bin/openSUSE-appstream-process'
 
 
-# Create the main susedata.xml with translations, support, and disk usage information
+# Create the additional app stream files if content provides necessary meta data
 def create_appstream(rpmdir):
     if not os.path.exists(OPENSUSE_APPSTREAM_PROCESS):
         return
 
     run_helper(
-        [OPENSUSE_APPSTREAM_PROCESS, rpmdir, f'{rpmdir}/repodata'],
-        failmsg='adding AppStream meta data',
+        [OPENSUSE_APPSTREAM_PROCESS, rpmdir, f'{rpmdir}'],
+        failmsg='Creating AppStream meta data',
         verbose=verbose_level > 0,
     )
+
+    # Did the process create files?
+    main_file = f'{rpmdir}/appdata.xml.gz'
+    if not os.path.exists(main_file):
+       return
+
+    mr = ModifyrepoWrapper(
+        file=main_file,
+        mdtype='appdata',
+        directory=os.path.join(rpmdir, "repodata"),
+    )
+    mr.run_cmd()
+    os.unlink(main_file)
+
+    icon_file = f'{rpmdir}/appdata-icons.tar.gz'
+    if os.path.exists(icon_file):
+        mr = ModifyrepoWrapper(
+            file=icon_file,
+            mdtype='appdata-icons',
+            directory=os.path.join(rpmdir, "repodata"),
+        )
+        mr.run_cmd()
+        os.unlink(icon_file)
+
+    ignore_file = f'{rpmdir}/appdata-ignore.xml.gz'
+    if os.path.exists(ignore_file):
+        os.unlink(ignore_file)
