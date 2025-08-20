9c31dad3c56ef7f8fec3671208447e8a67673597
Adrian Schröter <adrian@suse.de>
2025-08-20 10:24:04 +0200

    Switch back to SHA-256 for CHECKSUMS file
    linuxrc expects it (boo#1247507)

--- src/productcomposer/createartifacts/createchecksumfile.py
+++ src/productcomposer/createartifacts/createchecksumfile.py
@@ -3,6 +3,9 @@ from ..utils.runhelper import run_helper
 from ..config import chksums_tool
 
 def create_checksums_file(maindir):
+    # Legacy linuxrc expect SHA 256 checksums, so don't follow global default here
+    toolcmd = 'sha256sum'
+
     with open(maindir + '/CHECKSUMS', 'a') as chksums_file:
         for subdir in ('boot', 'EFI', 'docu', 'media.1'):
             if not os.path.exists(maindir + '/' + subdir):
@@ -11,5 +14,5 @@ def create_checksums_file(maindir):
                 for name in filenames:
                     relname = os.path.relpath(root + '/' + name, maindir)
                     run_helper(
-                        [chksums_tool, relname], cwd=maindir, stdout=chksums_file
+                        [toolcmd, relname], cwd=maindir, stdout=chksums_file
                     )
