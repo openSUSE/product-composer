349346b19a2bb9480fd5a2075e29306f1e28dc89
Adrian Schröter <adrian@suse.de>
2026-04-27 09:56:31 +0200

    Flush output lines to get correct timestamps

--- src/productcomposer/utils/loggerutils.py
+++ src/productcomposer/utils/loggerutils.py
@@ -3,16 +3,16 @@ from typing import NoReturn, Optional
 
 def die(msg: Optional[str], details: Optional[str] = None) -> NoReturn:
     if msg:
-        print("ERROR: " + msg)
+        print("ERROR: " + msg, flush=True)
     if details:
-        print(details)
+        print(details, flush=True)
     raise SystemExit(1)
 
 
 def warn(msg: str, details: Optional[str] = None) -> None:
-    print("WARNING: " + msg)
+    print("WARNING: " + msg, flush=True)
     if details:
-        print(details)
+        print(details, flush=True)
 
 def note(msg):
-    print(msg)
+    print(msg, flush=True)
